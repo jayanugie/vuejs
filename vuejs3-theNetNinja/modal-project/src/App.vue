1450497d92ed7c1e08079045402ba6c1a78090c9
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-10 00:44:57 +0700

    try multiple component

--- vuejs3-theNetNinja/modal-project/src/App.vue
+++ vuejs3-theNetNinja/modal-project/src/App.vue
@@ -2,13 +2,16 @@
   <h1>{{ title }}</h1>
   <input type="text" ref="name" />
   <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
 
 export default {
   name: "App",
+  components: { Modal },
   data() {
     return {
       title: "My First Vue App :)",
