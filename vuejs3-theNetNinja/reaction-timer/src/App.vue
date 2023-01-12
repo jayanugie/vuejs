951ce6dbb91bb7d9c2f5e3e0a2b4985082613783
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-12 20:03:30 +0700

    create a timer

--- vuejs3-theNetNinja/reaction-timer/src/App.vue
+++ vuejs3-theNetNinja/reaction-timer/src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"></Block>
+  <p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
+import Block from "./components/Block.vue";
 
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ vuejs3-theNetNinja/reaction-timer/src/components/Block.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    // console.log("component mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+      //   console.log(this.delay)
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+    //   console.log(this.reactionTime);
+    this.$emit('end', this.reactionTime);
+    },
+  },
+  //   updated() {
+  //     console.log('component updated')
+  //   },
+  //   unmounted() {
+  //     console.log('unmounted')
+  //   }
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
