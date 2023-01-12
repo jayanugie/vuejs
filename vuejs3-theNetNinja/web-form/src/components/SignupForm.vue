15e3a77fd0e73d51e3ad979d0ac5460ba247439b
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-12 22:23:23 +0700

    submit event, validation password

--- vuejs3-theNetNinja/web-form/src/components/SignupForm.vue
+++ vuejs3-theNetNinja/web-form/src/components/SignupForm.vue
@@ -1,10 +1,14 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
 
     <label>Password:</label>
     <input type="password" required v-model="password" />
+
+    <div v-if="passwordError" class="error">
+      {{ passwordError }}
+    </div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -25,6 +29,10 @@
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
   </form>
 
   <p>Email: {{ email }}</p>
@@ -44,6 +52,7 @@ export default {
       names: [],
       tempSkill: "",
       skills: [],
+      passwordError: "",
     };
   },
   methods: {
@@ -60,6 +69,21 @@ export default {
       this.skills = this.skills.filter((item) => {
         return skill !== item;
       });
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 chars long";
+
+      if (!this.passwordError) {
+        console.log("email:", this.email);
+        console.log("password:", this.password);
+        console.log("role:", this.role);
+        console.log("skills:", this.skills);
+        console.log("terms accepted:", this.terms);
+      }
     },
   },
 };
@@ -110,5 +134,22 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8rem;
+  font-weight: bold;
 }
 </style>
