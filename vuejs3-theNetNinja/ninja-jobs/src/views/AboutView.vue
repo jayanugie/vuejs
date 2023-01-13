1a3ca08401ee31cb10ee1909f2111601f11706d6
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-13 23:40:48 +0700

    router links

--- vuejs3-theNetNinja/ninja-jobs/src/views/AboutView.vue
+++ vuejs3-theNetNinja/ninja-jobs/src/views/AboutView.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dicta
+      quia placeat odio, delectus dignissimos eveniet laboriosam, iste,
+      similique iusto eaque quisquam! Cum assumenda labore est quam hic soluta
+      quaerat quos quasi reprehenderit sapiente, excepturi, quibusdam nesciunt
+      nostrum impedit voluptas tempore nemo laudantium velit voluptate
+      accusantium iusto eum possimus facilis! Necessitatibus repellat eos
+      architecto? Facilis accusamus, quisquam ab labore obcaecati illum delectus
+      iusto autem! Iure magni dignissimos sequi natus rerum error vitae
+      perferendis commodi reiciendis, a consequatur quam mollitia itaque
+      architecto recusandae hic amet nesciunt eaque tenetur? Cupiditate,
+      provident. Rerum, corporis. Sed ipsa praesentium quidem in corrupti,
+      dolorem vel explicabo!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dicta
+      quia placeat odio, delectus dignissimos eveniet laboriosam, iste,
+      similique iusto eaque quisquam! Cum assumenda labore est quam hic soluta
+      quaerat quos quasi reprehenderit sapiente, excepturi, quibusdam nesciunt
+      nostrum impedit voluptas tempore nemo laudantium velit voluptate
+      accusantium iusto eum possimus facilis! Necessitatibus repellat eos
+      architecto? Facilis accusamus, quisquam ab labore obcaecati illum delectus
+      iusto autem! Iure magni dignissimos sequi natus rerum error vitae
+      perferendis commodi reiciendis, a consequatur quam mollitia itaque
+      architecto recusandae hic amet nesciunt eaque tenetur? Cupiditate,
+      provident. Rerum, corporis. Sed ipsa praesentium quidem in corrupti,
+      dolorem vel explicabo!
+    </p>
   </div>
 </template>
--- vuejs3-theNetNinja/ninja-jobs/src/views/HomeView.vue
+++ vuejs3-theNetNinja/ninja-jobs/src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil eius
+      quibusdam placeat dolore ipsa. Ipsa magnam aut perferendis. Voluptatibus,
+      dignissimos minus tempora unde quam ratione harum assumenda deserunt, ut
+      enim vel nemo illo magnam nobis? Reprehenderit, dignissimos assumenda.
+      Corporis harum officiis nostrum in odit doloremque autem nobis rem itaque
+      nihil, animi repellendus quisquam aliquam libero dolore sapiente culpa?
+      Quisquam suscipit unde ut mollitia aperiam magnam veritatis voluptatum
+      quis. Impedit itaque expedita illo, nesciunt ipsum odio velit eligendi
+      maxime repudiandae. Molestias enim aliquam ab reprehenderit aspernatur
+      nesciunt iure culpa blanditiis dignissimos? Unde voluptas error
+      architecto. Officia temporibus ullam saepe commodi soluta.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil eius
+      quibusdam placeat dolore ipsa. Ipsa magnam aut perferendis. Voluptatibus,
+      dignissimos minus tempora unde quam ratione harum assumenda deserunt, ut
+      enim vel nemo illo magnam nobis? Reprehenderit, dignissimos assumenda.
+      Corporis harum officiis nostrum in odit doloremque autem nobis rem itaque
+      nihil, animi repellendus quisquam aliquam libero dolore sapiente culpa?
+      Quisquam suscipit unde ut mollitia aperiam magnam veritatis voluptatum
+      quis. Impedit itaque expedita illo, nesciunt ipsum odio velit eligendi
+      maxime repudiandae. Molestias enim aliquam ab reprehenderit aspernatur
+      nesciunt iure culpa blanditiis dignissimos? Unde voluptas error
+      architecto. Officia temporibus ullam saepe commodi soluta.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
