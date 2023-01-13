db9283ee14e9b65414a7097a0f7207e62ae9b247
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-13 23:53:11 +0700

    folder structure

--- vuejs3-theNetNinja/ninja-jobs/src/router/index.js
+++ vuejs3-theNetNinja/ninja-jobs/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import About from "../views/AboutView.vue";
+import Jobs from "../views/jobs/Jobs.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/about",
     name: "about",
     component: About,
+  },
+  {
+    path: "/jobs",
+    name: "jobs",
+    component: Jobs,
   },
 ];
 
