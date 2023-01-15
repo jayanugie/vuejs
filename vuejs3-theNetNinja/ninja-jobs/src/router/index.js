666e99ad9bf149148636ccd98bfd581ffe785b1c
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-15 21:07:04 +0700

    redirects & 404

--- vuejs3-theNetNinja/ninja-jobs/src/router/index.js
+++ vuejs3-theNetNinja/ninja-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import About from "../views/AboutView.vue";
 import Jobs from "../views/jobs/Jobs.vue";
 import JobDetails from "../views/jobs/JobDetails.vue";
+import NotFound from "../views/NotFound.vue";
 
 const routes = [
   {
@@ -25,6 +26,17 @@ const routes = [
     name: "jobDetails",
     component: JobDetails,
     props: true,
+  },
+  // redirect
+  {
+    path: "/all-jobs",
+    redirect: "/jobs",
+  },
+  // catchall 404
+  {
+    path: "/:catchAll(.*)",
+    name: "notFound",
+    component: NotFound,
   },
 ];
 
