688d96aceaac1bcadc0c71e35568fd4dfdaf4b42
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-15 21:20:31 +0700

    programmatic navigation

--- vuejs3-theNetNinja/ninja-jobs/src/App.vue
+++ vuejs3-theNetNinja/ninja-jobs/src/App.vue
@@ -4,8 +4,29 @@
     <router-link :to="{ name: 'about' }">About</router-link> |
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
   </nav>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: "home" });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -31,5 +52,17 @@ nav a {
 nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+}
+
+button:hover {
+  cursor: pointer;
+  background: #ddd;
 }
 </style>
