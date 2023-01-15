f704ff3b97887ae4d8bda15667ee1f39af55e35f
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-15 23:18:02 +0700

    conditionally showing data

--- vuejs3-theNetNinja/ninja-jobs/src/views/jobs/JobDetails.vue
+++ vuejs3-theNetNinja/ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,7 +1,12 @@
 <template>
-  <h1>{{ job.title }}</h1>
-  <p>The job id is {{ id }}</p>
-  <p>{{ job.details }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
 </template>
 
 <script>
@@ -20,7 +25,7 @@ export default {
   mounted() {
     fetch("http://localhost:3000/jobs/" + this.id)
       .then((res) => res.json())
-      .then((data) => (this.jobs = data))
+      .then((data) => (this.job = data))
       .catch((err) => console.log(err.message));
   },
 };
