0332736e23fee88459c76b8f80a27d54a122948e
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-16 21:15:48 +0700

    template refs (again)

--- vuejs3-theNetNinja/gie-blog/src/views/HomeView.vue
+++ vuejs3-theNetNinja/gie-blog/src/views/HomeView.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
   setup() {
-    console.log("setup");
+    console.log(this);
+
+    const p = ref("hai");
+    console.log(p, p.value);
 
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked me");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "hello world";
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
 };
 </script>
