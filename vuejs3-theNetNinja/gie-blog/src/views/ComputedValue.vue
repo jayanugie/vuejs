6545b09fc885b5640c0efe7526a8cbfdb3bc9511
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-16 22:54:37 +0700

    watch and watchEffect

--- vuejs3-theNetNinja/gie-blog/src/views/ComputedValue.vue
+++ vuejs3-theNetNinja/gie-blog/src/views/ComputedValue.vue
@@ -6,10 +6,11 @@
     <div v-for="name in matchingNames" :key="name">
       {{ name }}
     </div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, watch, watchEffect } from "vue";
 
 export default {
   name: "ComputedValue",
@@ -25,11 +26,24 @@ export default {
       "peach",
     ]);
 
+    const stopWatch = watch(search, () => {
+      console.log("watch function ran");
+    });
+
+    const stopEffect = watchEffect(() => {
+      console.log("watch effect function ran", search.value);
+    });
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value));
     });
 
-    return { names, search, matchingNames };
+    const handleClick = () => {
+      stopWatch();
+      stopEffect();
+    };
+
+    return { names, search, matchingNames, handleClick };
   },
 };
 </script>
