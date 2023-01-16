9bf1f51ef37445c7f91600bbaadf5a8bd6483f82
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-16 22:35:00 +0700

    computed values (search feature)

--- vuejs3-theNetNinja/gie-blog/src/router/index.js
+++ vuejs3-theNetNinja/gie-blog/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ComputedValue from "../views/ComputedValue.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/computed",
+    name: "computed",
+    component: ComputedValue,
   },
 ];
 
---
+++ vuejs3-theNetNinja/gie-blog/src/views/ComputedValue.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Computed</h1>
+    <input type="text" v-model="search" />
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
+  </div>
+</template>
+<script>
+import { ref, computed } from "vue";
+
+export default {
+  name: "ComputedValue",
+  setup() {
+    const search = ref("");
+    const names = ref([
+      "mario",
+      "yoshi",
+      "luigi",
+      "toad",
+      "bowser",
+      "koopa",
+      "peach",
+    ]);
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    return { names, search, matchingNames };
+  },
+};
+</script>
