1ce3f82c16e68a47587498d591703520e9bad617
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-02-07 11:49:57 +0700

    responsive hero component

--- vuejs2/kulineran/src/components/Hero.vue
+++ vuejs2/kulineran/src/components/Hero.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="row mt-4">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2><strong>Delicious Food Menu, </strong><br />in Your Gadget</h2>
-          <p>Ayo segera pilih dan pesan makanan favorit Anda.</p>
-          <router-link to="/foods" class="btn btn-lg btn-success">
-            <b-icon-arrow-right></b-icon-arrow-right> Pesan
-          </router-link>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong><br />in Your Gadget
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorit Anda.</p>
+              <router-link to="/foods" class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" alt="hero" width="100%" />
         </div>
       </div>
     </div>
-    <div class="col">
-      <img src="../assets/images/hero.png" alt="hero" width="100%" />
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/hero.png" alt="hero" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong><br />in Your Gadget
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorit Anda.</p>
+              <router-link to="/foods" class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
